0ecf598e2230eb7cb79b1695aa729f6fa6290f00
TheImperial <bobo201345@gmail.com>
2019-03-14 10:37:44 +0100

    Updates

---
+++ precache-manifest.3eaa1bd8bbba8bed58683b1f37be5c37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bd7a6dd00d0b7b08c3a1",
+    "url": "/static/js/main.08ed4c1e.chunk.js"
+  },
+  {
+    "revision": "4fb60d2ab2a1de7d95b6",
+    "url": "/static/js/2.914cacf8.chunk.js"
+  },
+  {
+    "revision": "bd7a6dd00d0b7b08c3a1",
+    "url": "/static/css/main.e2976f7e.chunk.css"
+  },
+  {
+    "revision": "28a5240e3212ccf0664dfae963674c42",
+    "url": "/index.html"
+  }
+];
